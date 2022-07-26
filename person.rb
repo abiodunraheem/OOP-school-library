2a640b705f7f0656328b9e0b4a5d7551a31a1600
abiodunraheem <raheemabiodun23@gmail.com>
2022-07-26 16:47:52 +0100

    Update person.rb file

--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
 class Person 
-    attr_accessor :id, :name, :age
+    attr_reader :id
+    attr_accessor :name, :age
     def initialize (age, name: "unknown", parent_permission: true)
         @id = Random.rand(1..1000)
         @age = age

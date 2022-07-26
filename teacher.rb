1679a0970a2d21178cae40a29c6d5ff3d7d488da
abiodunraheem <raheemabiodun23@gmail.com>
2022-07-26 19:41:25 +0100

    Update specialization to teacher.rb file and fix linter errors

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,14 @@
 require_relative './person'
 
 class Teacher < Person
-    def initialize (specialization, age, name: "unknown", parent_permission: true)
-        super(age, name: name, parent_permission: parent_permission)
-        @specialization = specialization
-    end
+  attr_reader :specialization
 
-    def can_use_services?
-        true
-    end
+  def initialize(specialization, age, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
